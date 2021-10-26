e0eb397214948492b177a4352a6c72ffcd09c97f
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-10-26 16:30:31 +0300

    Project started

---
+++ Find Your Hat/main.js
@@ -0,0 +1,26 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {
+    constructor(field) {
+        this.field = field;
+    }
+    print() {
+        this.field.forEach(element => {
+            console.log(element.join(''));
+        });
+    }
+}
+
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+]);
+
+const command = prompt('Input command: ');
+console.log(command);
