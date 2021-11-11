d1ea1ce986072e33670b8ad0080a62ab384dc772
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-11-11 15:52:51 +0200

    Added search results of tracks

--- jamming/src/Components/Track/Track.js
+++ jamming/src/Components/Track/Track.js
@@ -1,16 +1,18 @@
 import React from "react";
 import './Track.css';
 
-export class Track extends React.Component {
+class Track extends React.Component {
     render() {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3>{/* track name will go here */}</h3>
-                    <p>{/* track artist will go here */}</p>
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
                 <button className="Track-action">{/* + or - will go here */}</button>
             </div>
         );
     }
 }
+
+export default Track;
