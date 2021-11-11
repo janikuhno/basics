5aa634b1100e4657d2601ceaf9bd3dd3fc6f8e79
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-11-11 13:42:53 +0200

    Updated App, SearchBar

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-/* import { SearchBar } from '../Components/SearchBar'; */
+import { SearchBar } from '../SearchBar';
 /* import { SearchResults } from '../Components/SearchResults'; */
 /* import { Playlist } from '../Components/Playlist'; */
 
@@ -10,7 +10,7 @@ class App extends React.Component {
       <div>
         <h1>Ja<span className="highlight">mm</span>ing</h1>
         <div className="App">
-          {/* Add a SearchBar component */}
+          <SearchBar />
           <div className="App-playlist">
             {/* Add a SearchResults component */}
             {/* Add a Playlist component */}
