2eaa0de2e8381d1544e1209568d773956d548a7d
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-11-11 15:23:20 +0200

    Added TrackList and Track

--- jamming/src/Components/Playlist/Playlist.js
+++ jamming/src/Components/Playlist/Playlist.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './Playlist.css';
-/* import { Tracklist } from ''; */
+/* import { TrackList } from ''; */
 
 export class Playlist extends React.Component {
     render() {
         return(
             <div className="New Playlist">
                 <input defaultValue={'New Playlist'}/>
-                {/* Add a Tracklist component */}
+                {/* Add a TrackList component */}
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         );
--- jamming/src/Components/SearchResults/SearchResults.js
+++ jamming/src/Components/SearchResults/SearchResults.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './SearchResults.css';
-/* import { Tracklist } from ''; */
+/* import { TrackList } from ''; */
 
 export class SearchResults extends React.Component {
     render() {
         return(
             <div className="SearchResults">
                 <h2>Results</h2>
-                {/* Add a Tracklist component */}
+                {/* Add a TrackList component */}
             </div>
         )
     }
